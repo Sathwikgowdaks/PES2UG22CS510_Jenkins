882568b62a5fc93082474c00fb9dc44efba41b90
sathwik <sathwik@sathwiks-MacBook-Air-2.local>
2025-03-11 15:25:35 +0530

    first commite

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout <<"Sathwik Gowda k s PES2UG22CS510\n";
   return 0;
 }
